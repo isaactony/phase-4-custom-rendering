8dbb1005905d6bd860e5090dc09dae9d3814d4ae
isaac <isaactonyloi@gmail.com>
2023-03-17 11:43:55 +0300

    except and only in rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,27 @@
 class CheesesController < ApplicationController
+  def summary
+    "#{name}: $#{price}"
+  end
 
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    if cheeses
+      render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }
+    end
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
 
 end
